d899353c4045971132ff483db93feb4a0b0a0499
Aoi K <62386689+aoikurokawa@users.noreply.github.com>
2025-11-03 07:20:43 +0900

    Stake Pool Stats (#17)
    * feat: stake pool stats
    
    * feat: bump up
    
    * feat: update

---
+++ examples/stake_pool_stats.rs
@@ -0,0 +1,28 @@
+use std::time::Duration;
+
+use chrono::Utc;
+use kobe_client::{client_builder::KobeApiClientBuilder, types::StakePoolStatsRequest};
+
+#[tokio::main]
+async fn main() {
+    let client = KobeApiClientBuilder::new()
+        .timeout(Duration::from_secs(45))
+        .retry(true)
+        .max_retries(5)
+        .build();
+
+    let current_epoch = client.get_current_epoch().await.unwrap();
+    println!("Current epoch: {}\n", current_epoch);
+
+    let end = Utc::now();
+
+    let start = end - chrono::Duration::days(1);
+    let request = StakePoolStatsRequest::new()
+        .with_bucket_type("Daily")
+        .with_range_filter(start, end)
+        .sort_asc();
+
+    let stake_pool_stats = client.get_stake_pool_stats(Some(&request)).await.unwrap();
+
+    println!("Stake Pool Stats: {:?}", stake_pool_stats);
+}
