435146926af83c2f8806368f9d88c45efe10b5d5
aoikurokawa <ht.so.ik0102@gmail.com>
2025-11-14 12:10:50 +0900

    feat: update

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,8 @@ pub mod types;
 /// Default base URL for mainnet Kobe API
 pub const MAINNET_BASE_URL: &str = "https://kobe.mainnet.jito.network";
 
+/// Default base URL for testnet Kobe API
+pub const TESTNET_BASE_URL: &str = "https://kobe.testnet.jito.network";
+
 /// API version
 pub const API_VERSION: &str = "v1";
