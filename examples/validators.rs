d9f9f4ac062b1e9c60fe46b440d8009da5a02f35
Aoi K <62386689+aoikurokawa@users.noreply.github.com>
2026-06-20 02:25:08 +0900

    Add `bam_connection_rate`, `identity_account` (#25)
    * fix: add bam_connection_rate
    
    * fix: update

--- examples/validators.rs
+++ examples/validators.rs
@@ -10,10 +10,23 @@ async fn main() {
         .max_retries(5)
         .build();
 
-    let current_epoch = client.get_current_epoch().await.unwrap();
+    let current_epoch = 989;
     println!("Current epoch: {}\n", current_epoch);
 
     let validators = client.get_validators(Some(current_epoch)).await.unwrap();
 
     println!("Found {} validators", validators.validators.len());
+
+    if let Some(first) = validators.validators.first() {
+        println!("First validator: {:?}", first);
+    }
+
+    let mut count = 0;
+    for validator in validators.validators {
+        if validator.bam_connection_rate.is_some() {
+            count += 1;
+        }
+    }
+
+    println!("Count: {count}");
 }
